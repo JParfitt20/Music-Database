4c28f2c9a68a8f45f1981fab969e5c31799dd3e1
JParfitt20 <no-reply>
2019-03-02 15:31:54 +0000

    sorting by list and grade added

--- inc/sections.php
+++ inc/sections.php
@@ -2,6 +2,7 @@
 // Setting section for page
 $section = null;
 
+include('subsections.php');
 
 if(isset($_GET['instrument'])){
   $output = array();
@@ -9,49 +10,15 @@ if(isset($_GET['instrument'])){
     $section = "Piano";
   } else if ($_GET['instrument'] == 'flute'){
     $section = "Flute";
-  }
-  foreach ($music as $item){
+  } else {$output=$grade_subsection;}
+  foreach ($grade_subsection as $item){
     if (strtolower($item['Instrument']) == strtolower($section)){
       $output[] = $item;
     }
   }
 } else {
-  $output = $music;
+  $output = $grade_subsection;
 
 }
 
-
-
-
-// if(isset($_GET['grade'])){
-//   $output_2 = array();
-//   if ($_GET['grade'] == "1"){
-//     $grade = 1;
-//   } else if ($_GET['grade'] == "2"){
-//     $grade = 2;
-//   }
-//   foreach ($output as $item){
-//     if ($item['Grade'] == $grade){
-//       $output_2[] = $item;
-//     }
-//   }
-//
-// }
-
-// if(isset($_GET['list'])){
-//   if ($_GET['list'] == 'a'){
-//     $section = "A";
-//   } else if ($_GET['list'] == 'b'){
-//     $section = "B";
-//   } else if ($_GET['list'] == 'c'){
-//     $section = "C";
-//   }
-//   foreach ($music as $item){
-//     if (strtolower($item['List']) == strtolower($section)){
-//       $output[] = $item;
-//     }
-//   }
-// }
-// ifse {
-//   $output=$music;}
 ?>
---
+++ inc/subsections.php
@@ -0,0 +1,27 @@
+<?php
+include ('data.php');
+
+if(isset($_GET['list'])){
+  $list = $_GET['list'];
+  $list_subsection = array();
+  foreach($music as $item){
+    if (strtolower($item['List']) == $list){
+      $list_subsection[]=$item;
+    }
+  }
+}else {$list_subsection=$music;}
+
+if (isset($_GET['grade'])){
+  $grade=$_GET['grade'];
+  $grade_subsection=array();
+  foreach($list_subsection as $item){
+    if ($item['Grade'] == $grade){
+      $grade_subsection[]=$item;
+    }
+  }
+} else {$grade_subsection=$list_subsection;}
+
+
+
+
+?>
